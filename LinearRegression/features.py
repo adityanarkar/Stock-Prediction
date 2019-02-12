c6a03aac972aabbb0daf42804fbd2e29f597919a
Aditya Narkar <aditya.narkar25@gmail.com>
2019-02-12 01:03:24 -0600

    discretized --> Momentum, %k, %d

--- LinearRegression/features.py
+++ LinearRegression/features.py
@@ -34,23 +34,38 @@ def EMA(df, moving_avg_window, close):
             temp = df.iloc[row - 1, -1]
             df.iloc[row, -1] = (df.iloc[row, close] - temp) * multiplier + temp
 
+
+def discretizeMomentum(df: pd.DataFrame, row, prev):
+    if prev != np.nan:
+        if df.iloc[row, -1] < prev:
+            prev = df.iloc[row, -1]
+            df.iloc[row, -1] = -1
+        else:
+            prev = df.iloc[row, -1]
+            df.iloc[row, -1] = 1
+    return prev
+
 def momentum(df, close):
+    prev = np.nan
     for row in range(9, len(df.index)):
         df.iloc[row, -1] = df.iloc[row, close] - df.iloc[row - 9, close]
-
+        prev = discretizeMomentum(df, row, prev)
 
 def stochasticK(df, close, high, low, window):
+    prev = np.nan
     for row in range(window, len(df.index)):
         currentClose = df.iloc[row, close]
         highestHigh = df.iloc[row - window:row, high].max()
         lowestLow = df.iloc[row - window:row, low].min()
-
         df.iloc[row, -1] = (currentClose - lowestLow) / (highestHigh - lowestLow)
+        prev = discretizeMomentum(df, row, prev)
 
 
 def stochasticD(df, K):
+    prev = np.nan
     for row in range(2, len(df.index)):
         df.iloc[row, -1] = df.iloc[row - 2:row, K].mean()
+        prev = discretizeMomentum(df, row, prev)
 
 
 def RSI(df: pd.DataFrame, close):
